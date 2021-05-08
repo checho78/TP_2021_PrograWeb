4d31c1cae1fd5f99ac8124fd4fa1d8eab4f3f40c
Diana Cruces Castillo <dianacruces_17@hotmail.com>
2021-05-07 19:27:23 -0500

    Pharmacy by DianaC

---
+++ tp/src/main/java/pe/edu/upc/service/IPharmacyService.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.service;
+
+import java.util.List;
+
+
+import pe.edu.upc.entity.Pharmacy;
+
+
+public interface IPharmacyService {
+
+	public void insert(Pharmacy  p);
+	public List<Pharmacy> list();
+	public List<Pharmacy> findByNamePharmacy(Pharmacy p);
+	public void delete(int idPharmacy); 
+}
